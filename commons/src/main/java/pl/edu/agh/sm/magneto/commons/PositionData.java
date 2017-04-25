03497a14e6178fa5cae9fe4f482f18a5ccc5a68b
Michal Suder <michalsuder@wp.pl>
2017-04-25 19:33:31 +0200

    Add position field to PositionData

--- commons/src/main/java/pl/edu/agh/sm/magneto/commons/PositionData.java
+++ commons/src/main/java/pl/edu/agh/sm/magneto/commons/PositionData.java
@@ -5,17 +5,67 @@ import org.apache.commons.lang3.SerializationUtils;
 import java.io.Serializable;
 
 public class PositionData implements Serializable {
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 2L;
 
     private final String value;
 
-    public PositionData(String value) {
-        this.value = value;
+    private final int x;
+
+    private final int y;
+
+    private final int z;
+
+    /**
+     * Create new PositionData instance.
+     *
+     * @param x right-left
+     * @param y up-down
+     * @param z front-back
+     */
+    public PositionData(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.value = x + ";" + y + ";" + z;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public String getValue() {
+        return value;
     }
 
     @Override
     public String toString() {
         return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PositionData that = (PositionData) o;
+
+        return x == that.x && y == that.y && z == that.z;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        result = 31 * result + z;
+        return result;
     }
 
     public static byte[] serialize(PositionData positionData) {
